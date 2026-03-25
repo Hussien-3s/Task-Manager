ee3b7aee78ab6b17fadf74015c5ec11eefc830e8
Hussien-3s <hussienksab7@gmail.com>
2026-03-25 17:28:48 +0200

    Refactor CardSmall to use CardContent and CardFooter

--- components/TeamCard.tsx
+++ components/TeamCard.tsx
@@ -8,6 +8,7 @@ import {
 import { Button } from "./ui/button"
 import { BookOpen } from 'lucide-react';
 import { useRouter } from 'next/navigation';
+import { CardContent, CardFooter } from "./ui/card";
 
 export function CardSmall({ teamEmail }: { teamEmail: string }) {
 
@@ -18,28 +19,27 @@ export function CardSmall({ teamEmail }: { teamEmail: string }) {
     }
 
     return (
-        <Card size="sm" className="mx-auto mb-5 w-full max-w-5xl flex flex-row">
-            <CardHeader>
-                <CardTitle className="flex items-center justify-between gap-5">
-                    <div>
-                        <div className="text-lg font-semibold">
-                            {teamEmail}
-                        </div>
-                        <div className="text-sm text-muted-foreground">
-                            admin
-                        </div>
-                    </div>
-                    <div>
-                        <Button
-                            type="button"
-                            onClick={handleReq}
-                            className="bg-black text-white w-12 h-12 rounded-full flex items-center justify-center hover:bg-blue-700"
-                        >
-                            <BookOpen className="h-6 w-6" />
-                        </Button>
-                    </div>
-                </CardTitle>
-            </CardHeader>
+        <Card className="mx-auto w-full mb-5 flex flex-row max-w-5xl p-0 items-center">
+            <CardContent className="space-y-4 text-left p-5 flex-1">
+                <div className="text-lg font-semibold">
+                    {teamEmail}
+                </div>
+                <div className="text-sm text-muted-foreground">
+                    admin
+                </div>
+            </CardContent>
+
+            <CardFooter className="p-5 bg-#737373 border-0">
+                <div>
+                    <Button
+                        type="button"
+                        onClick={handleReq}
+                        className="bg-black text-white w-12 h-12 rounded-full flex items-center justify-center hover:bg-blue-700"
+                    >
+                        <BookOpen className="h-6 w-6" />
+                    </Button>
+                </div>
+            </CardFooter>
         </Card>
     )
 }
